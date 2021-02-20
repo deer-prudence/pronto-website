1d50c414b3cce7d65b4d61ce7e64ab6d1c511ccf
EC2 Default User <ec2-user@ip-172-31-6-153.us-east-2.compute.internal>
2021-02-20 17:01:28 +0000

    basic stripe implementation

--- api/api.py
+++ api/api.py
@@ -1,8 +1,31 @@
-import time
-from flask import Flask
+import stripe
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+  session = stripe.checkout.Session.create(
+    payment_method_types=['card'],
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='https://example.com/success',
+    cancel_url='https://example.com/cancel',
+  )
+
+  return jsonify(id=session.id)
+
+if __name__== '__main__':
+    app.run(port=4242)
