c07a44fc5e260a0227e3ffcfcf74697071227d30
EC2 Default User <ec2-user@ip-172-31-6-153.us-east-2.compute.internal>
2021-03-09 04:32:37 +0000

    index page design begun

--- api/api.py
+++ api/api.py
@@ -1,31 +1,30 @@
 import stripe
+import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
 
-@app.route('/create-checkout-session', methods=['POST'])
-def create_checkout_session():
-  session = stripe.checkout.Session.create(
-    payment_method_types=['card'],
-    line_items=[{
-      'price_data': {
-        'currency': 'usd',
-        'product_data': {
-          'name': 'T-shirt',
-        },
-        'unit_amount': 2000,
-      },
-      'quantity': 1,
-    }],
-    mode='payment',
-    success_url='https://example.com/success',
-    cancel_url='https://example.com/cancel',
-  )
 
-  return jsonify(id=session.id)
+@app.route('/create-payment-intent/<amount>', methods=['POST'])
+def create_payment(amount):
+    try:
+        print("here")
+        print(amount)
+        intent = stripe.PaymentIntent.create(
+            amount=20,
+            currency='usd'
+        )
+        return jsonify({
+          'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        print("exception")
+        print(e)
+        return jsonify(error=str(e)), 403
+
 
 if __name__== '__main__':
     app.run(port=4242)
